3bb8f664255a8654eed4aeb92904816b766646e3
Niraj Pant <nirajpant7@gmail.com>
2014-08-19 01:06:26 -0500

    added url linking

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('androidtoolboxApp')
   .controller('MainCtrl', function ($scope, $http) {
 
     $scope.list = [{"name":"Estimote Android SDK","category":"Sensors","link":"https://github.com/Estimote/Android-SDK"},
-    {"name":"Picasso","nategory":"Images","link":"https://github.com/square/picasso"}];
+    {"name":"Picasso","category":"Images","link":"https://github.com/square/picasso"}];
 
     // $http.get("./libraries.json").success(function (data) {
     //     $scope.list = data;
