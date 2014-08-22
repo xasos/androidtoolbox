4df72eba6676c472710a4b4ec05fa4277b9493e3
Niraj Pant <nirajpant7@gmail.com>
2014-08-22 16:37:37 -0500

    fixed json capitalization

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,12 @@ angular.module('androidtoolboxApp')
   .controller('MainCtrl', function ($scope, $http) {
 
     $scope.list = [{"name":"Estimote Android SDK","category":"Sensors","link":"https://github.com/Estimote/Android-SDK"},
-    {"name":"Picasso","category":"Images","link":"https://github.com/square/picasso"}];
-
+    {"name":"Picasso","category":"Images","link":"https://github.com/square/picasso"},
+    {"name":"StandOut","category":"Services","link":"https://github.com/pingpongboss/StandOut"},
+    {"name":"GSON","category":"Data","link":"https://code.google.com/p/google-gson"},
+    {"name":"Retrofit","category":"Data","link":"http://square.github.io/retrofit/"},
+    {"name":"Otto","category":"Optimization","link":"https://github.com/square/otto"},
+    {"name":"Android-Beacon-Library","category":"Sensors","link":"https://github.com/AltBeacon/android-beacon-library"}];
     // $http.get("./libraries.json").success(function (data) {
     //     $scope.list = data;
     // });
